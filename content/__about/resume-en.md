e1ce56487e9fb8d076ca306ba089b13566a9a08d
DAHEE JEONG <unchd26@gmail.com>
2021-01-18 14:59:58 +0900

    setting: create blog

---
+++ content/__about/resume-en.md
@@ -0,0 +1,13 @@
+---
+title: 'about'
+date: 2021-1-18 13:00:00
+lang: 'en'
+---
+
+# Jeong Dahee
+
+<div align="left">
+
+_Junior Front-end Developer_
+
+</div>
---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `LIGHT ON DEV`,
+  description: `Junior Front-end Developer`,
+  author: `Hee`,
+  introduction: `🚀𝙏𝙤 𝙞𝙣𝙛𝙞𝙣𝙞𝙩𝙮 𝙖𝙣𝙙 𝙗𝙚𝙮𝙤𝙣𝙙 ✨ Junior Front-end Developer`,
+  siteUrl: `https://eternalsunshine.netlify.app`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `eternalarchive`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: `100048971984783`, // Your Facebook account
+    linkedin: `dahee-jeong-4b271a1b9`, // Your LinkedIn account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`devlog`, `HTML`, `CSS`, `JavaScript`, `React`, `Frontend`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'eternalarchive/blog-comments', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: '',
+  },
+  share: {
+    facebookAppId: '721871498723066', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: 'UA-162820540-1', // Add your google analytics tranking ID
+}
